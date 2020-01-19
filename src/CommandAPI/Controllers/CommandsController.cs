86576b704bde75e4fc6cce19a48e152bcc7f3cfd
Les Jackson <les.jackson@binarythistle.com>
2020-01-19 13:01:47 +1100

    Initial Commit

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] {"this", "is", "hard", "coded"};
+        }
+    }
+}
